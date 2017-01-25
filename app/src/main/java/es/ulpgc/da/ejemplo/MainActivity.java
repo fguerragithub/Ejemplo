c161825d60221b1f24771d4a03ad7b71e0538374
fguerra <francisco.guerra@ulpgc.es>
2017-01-25 11:54:51 +0000

    Primer ejemplo funcionado de un contador

--- app/src/main/java/es/ulpgc/da/ejemplo/MainActivity.java
+++ app/src/main/java/es/ulpgc/da/ejemplo/MainActivity.java
@@ -17,54 +17,13 @@ public class MainActivity extends Activity {
     private TextView display;
     private Button boton1;
     private int contador;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
 
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    public Action getIndexApiAction() {
-        Thing object = new Thing.Builder()
-                .setName("Main Page") // TODO: Define a title for the content shown.
-                // TODO: Make sure this auto-generated URL is correct.
-                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
-                .build();
-        return new Action.Builder(Action.TYPE_VIEW)
-                .setObject(object)
-                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
-                .build();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        AppIndex.AppIndexApi.start(client, getIndexApiAction());
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        AppIndex.AppIndexApi.end(client, getIndexApiAction());
-        client.disconnect();
-    }
-
-    class BotonListener implements View.OnClickListener {
+    public class BotonListener implements View.OnClickListener {
 
         @Override
         public void onClick(View v) {
             contador++;
-            display.setText(contador);
+            display.setText(((Integer)contador).toString());
         }
     }
 
@@ -77,5 +36,6 @@ public class MainActivity extends Activity {
         contador = 0;
         boton1 = (Button)findViewById(R.id.boton1);
         display = (TextView)findViewById(R.id.display);
+        boton1.setOnClickListener(new BotonListener());
     }
 }
